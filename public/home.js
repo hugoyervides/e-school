ba8efa851178e57ab8f422be7db17b32e06cbd4d
Luis Zul <luiszul@Luiss-MacBook-Pro.local>
2020-01-22 23:40:11 -0600

    Navbar from landing page now is a VueJS component

--- public/home.js
+++ public/home.js
@@ -1,6 +1,47 @@
-var appNavBar = new Vue(***REMOVED***
-  el: '#appNavBar',
+var appCompanyTitle = new Vue(***REMOVED***
+  el: '#appCompanyTitle',
   data: ***REMOVED***
+    company: "E-School"
+  ***REMOVED***
+***REMOVED***);
+
+Vue.component('navcomponent',***REMOVED***
+  template : `<nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item">
+        <img v-bind:src="logo" width="auto">
+      </a>
+
+      <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a v-for="item in navbarItems" class="navbar-item">
+          ***REMOVED******REMOVED*** item.text ***REMOVED******REMOVED***
+        </a>
+        </div>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>***REMOVED******REMOVED*** signup ***REMOVED******REMOVED***</strong>
+            </a>
+            <a class="button is-light">
+              ***REMOVED******REMOVED*** login ***REMOVED******REMOVED***
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>`,
+  data: function() ***REMOVED*** return ***REMOVED***
     logo: "./static/img/google.png",
     navbarItems: [
       ***REMOVED***text: "Home" ***REMOVED***,
@@ -8,15 +49,62 @@ var appNavBar = new Vue(***REMOVED***
     ],
     signup: "Sign Up",
     login: "Log In"
+    ***REMOVED***
   ***REMOVED***
 ***REMOVED***);
 
-var appCompanyTitle = new Vue(***REMOVED***
-  el: '#appCompanyTitle',
-  data: ***REMOVED***
-    company: "E-School"
-  ***REMOVED***
+var navbarApp = new Vue(***REMOVED***
+  el: '#appNavBar'
 ***REMOVED***);
+
+Vue.component("navcomponent", ***REMOVED***
+  template: `<nav class="navbar" role="navigation" aria-label="main navigation">
+              <div class="navbar-brand">
+                <a class="navbar-item">
+                  <img v-bind:src="logo" width="auto">
+                </a>
+
+                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                </a>
+              </div>
+              
+              <div id="navbarBasicExample" class="navbar-menu">
+                <div class="navbar-start">
+                  <a v-for="item in navbarItems" class="navbar-item">
+                    ***REMOVED******REMOVED*** item.text ***REMOVED******REMOVED***
+                  </a>
+                  </div>
+                </div>
+
+                <div class="navbar-end">
+                  <div class="navbar-item">
+                    <div class="buttons">
+                      <a class="button is-primary">
+                        <strong>***REMOVED******REMOVED*** signup ***REMOVED******REMOVED***</strong>
+                      </a>
+                      <a class="button is-light">
+                        ***REMOVED******REMOVED*** login ***REMOVED******REMOVED***
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </nav>`,
+    data: function() ***REMOVED*** return ***REMOVED***
+      logo: "./static/img/google.png",
+      navbarItems: [
+        ***REMOVED***text: "Home" ***REMOVED***,
+        ***REMOVED***text: "Courses" ***REMOVED***,
+      ],
+      signup: "Sign Up",
+      login: "Log In"
+    ***REMOVED***
+  ***REMOVED***
+  ***REMOVED***
+);
 
 var appFeaturedCourses = new Vue(***REMOVED***
   el: "#appFeaturedCourses",
