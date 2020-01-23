25c60ef3ab6315b3d35576f98900cc0ae542a90b
Luis Zul <luiszul@Luiss-MacBook-Pro.local>
2020-01-22 23:21:19 -0600

    Connected Bulma CSS components with VueJS

--- public/home.js
+++ public/home.js
@@ -1,6 +1,55 @@
-var app = new Vue(***REMOVED***
-    el: '#app',
-    data: ***REMOVED***
-      message: 'Hello Vue!'
-    ***REMOVED***
-  ***REMOVED***)
+var appNavBar = new Vue(***REMOVED***
+  el: '#appNavBar',
+  data: ***REMOVED***
+    logo: "./static/img/google.png",
+    navbarItems: [
+      ***REMOVED***text: "Home" ***REMOVED***,
+      ***REMOVED***text: "Courses" ***REMOVED***,
+    ],
+    signup: "Sign Up",
+    login: "Log In"
+  ***REMOVED***
+***REMOVED***);
+
+var appCompanyTitle = new Vue(***REMOVED***
+  el: '#appCompanyTitle',
+  data: ***REMOVED***
+    company: "E-School"
+  ***REMOVED***
+***REMOVED***);
+
+var appFeaturedCourses = new Vue(***REMOVED***
+  el: "#appFeaturedCourses",
+  data: ***REMOVED***
+    featuredCoursesTitle: "Featured Courses",
+    featuredCourses: [
+      ***REMOVED***
+        img: "./static/img/course1.jpg",
+        author: ***REMOVED***
+          name: "John Smith",
+          img: "./static/img/professor1.jpg",
+          title: "Entrepreneur"
+        ***REMOVED***,
+        description: "This is a sample course description." 
+      ***REMOVED***,
+      ***REMOVED***
+        img: "./static/img/course2.jpg",
+        author: ***REMOVED***
+          name: "Dahlia Hawthorne",
+          img: "./static/img/professor2.jpg",
+          title: "Entrepreneur"
+        ***REMOVED***,
+        description: "This is a sample course description." 
+      ***REMOVED***,
+      ***REMOVED***
+        img: "./static/img/course3.jpg",
+        author: ***REMOVED***
+          name: "Jessye Davis",
+          img: "./static/img/professor3.jpg",
+          title: "Consultant"
+        ***REMOVED***,
+        description: "This is a sample course description." 
+      ***REMOVED***,
+    ]
+  ***REMOVED***
+***REMOVED***)
