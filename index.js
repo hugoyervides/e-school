2614be13cf367cddc788c779bcb4e932b59e3312
Luis Zul <luiszul@Luiss-MacBook-Pro.local>
2020-01-23 23:10:15 -0600

    Enabled firebase connection again

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const {PORT} = require('./config');
 const express = require('express')
 const bodyParser = require("body-parser")
 const app = express();
-//const users = require("./public/routes/users")
+const users = require("./public/routes/users")
 
 app.use('/static', express.static('public'));
 
 app.use(bodyParser.json());
 
-//app.use("/api/", users)
+app.use("/api/", users)
 
 app.get('/', (req, res) => {
   return res.sendFile("home.html", {root: "public"});
