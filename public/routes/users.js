3276163350d7b6aec392fade8ebb8944db86ac2a
Luis Zul <luiszul@Luiss-MacBook-Pro.local>
2020-01-24 18:27:41 -0600

    Corrected key route

--- public/routes/users.js
+++ public/routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("../../serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
